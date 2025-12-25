16f420ac9f97c3ae95e75332690b160077a5febe
dataligtv-creator <dataligtv@gmail.com>
2025-12-25 16:34:27 +0300

    Update 4_🔥_Pressure_Lab.py

--- pages/4_🔥_Pressure_Lab.py
+++ pages/4_🔥_Pressure_Lab.py
@@ -1,3 +1,9 @@
+# Scout DNA sayfasında seçim yapınca:
+st.session_state['secilen_oyuncu'] = p1_name
+
+# Match Center sayfasında okurken:
+if 'secilen_oyuncu' in st.session_state:
+    varsayilan_oyuncu = st.session_state['secilen_oyuncu']
 import streamlit as st
 import matplotlib.pyplot as plt
 from mplsoccer import VerticalPitch
